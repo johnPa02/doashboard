4ed489f1fa931ee0edc16f4b73d09d3a5fdb7dd3
Phan Quoc Toan <phanqu6ctoan@gmail.com>
2022-07-08 14:56:20 +0700

    React Admin Dashboard

---
+++ src/pages/Employees.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GridComponent,Page,Inject,ColumnDirective,Search, Toolbar } from '@syncfusion/ej2-react-grids'
+
+import {employeesData, employeesGrid } from '../data/dummy'
+import { Header } from '../components'
+import { ColumnsDirective } from '@syncfusion/ej2-react-charts'
+
+const Employees = () => {
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Employees'/>
+      <GridComponent 
+      id ='gridcomp' 
+      dataSource={employeesData}
+      allowPaging
+      toolbar={['Search']}
+      width='auto'
+      >
+        <ColumnsDirective>
+        {employeesGrid.map((item,index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        <Inject services={[Page,Search,Toolbar]}/>
+      </GridComponent>
+    </div>
+  )
+}
+
+export default Employees
---
+++ src/pages/Orders.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GridComponent, Resize,Sort, ContextMenu,Filter,Page,ExcelExport,
+PdfExport,Edit,Inject,ColumnDirective } from '@syncfusion/ej2-react-grids'
+
+import { contextMenuItems, ordersData, ordersGrid } from '../data/dummy'
+import { Header } from '../components'
+import { ColumnsDirective } from '@syncfusion/ej2-react-charts'
+
+const Orders = () => {
+  return (
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders'/>
+      <GridComponent 
+      id ='gridcomp' 
+      dataSource={ordersData}
+      allowPaging
+      allowSorting>
+        <ColumnsDirective>
+        {ordersGrid.map((item,index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        <Inject services={[Resize,Sort,ContextMenu,Filter,Page,ExcelExport,Edit,
+        PdfExport]}/>
+      </GridComponent>
+    </div>
+  )
+}
+
+export default Orders
